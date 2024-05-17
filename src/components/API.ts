e2eb2fb9cb7f3495c17fc2bc59b88b2c03fb4728
Hellcat232 <maxkuich1993@gmail.com>
2024-05-18 01:11:55 +0300

    sdfsdf

--- src/components/API.ts
+++ src/components/API.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Photo } from "./types";
 
 axios.defaults.headers.common["Authorization"] =
   "Client-ID zr1Os87TpRzUJS7xGhKv3NSxWm-6AC-9zIcesBRS42c";
@@ -8,7 +9,13 @@ const params = {
   per_page: 20,
 };
 
-export const fetchApi = async (query, page) => {
+interface Data {
+  results: Photo[];
+  total: number;
+  total_pages: number;
+}
+
+export const fetchApi = async (query: string, page: number): Promise<Data> => {
   const { data } = await axios.get(
     `https://api.unsplash.com/search/photos?query=${query}&page=${page}`,
     { params }
